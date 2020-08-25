3edae4c6aa50f51c56fed1d6b9d7289179e9d211
Sparsh <sparsh.ailawadi@gmail.com>
2020-08-25 06:22:31 +0530

    token

--- src/bot.js
+++ src/bot.js
@@ -9,10 +9,15 @@ client.on("ready", () => {
 });
 
 var curse = [
-  "Suck a cock, ",
-  "fuck you, ",
-  "your mom is a whore, ",
-  "your dad should have worn a condom, "
+  "people like you should come with a return window, ",
+  "WULA comedians are funnier than you are, ",
+  "your sex life is worse than the life of prisoners at tihar ",
+  "your dad should have worn a condom, ",
+  "your presence on this server is really hurting the avg IQ around, ",
+  "I can bet your dad would trade you for a blow job, ",
+  "If you were anymore boring, your name would be a course code, ",
+  "Giving you a scholarship was the biggest mistake Shiv Nadar made, and he made Roshni, ",
+  "I so wish your dad didn't cheap out on the condoms, "
 ];
 
 client.on("message", message => {
@@ -24,11 +29,11 @@ client.on("message", message => {
       .substring(prefix.length)
       .split(/\s+/);
     if (cmdName == "curse") {
-      var rnd = Math.floor(Math.random() * 4 + 1);
+      var rnd = Math.floor(Math.random() * 10 + 1);
       rnd = rnd - 1;
       message.channel.send(`${curse[rnd]} ${args}`);
     }
   }
 });
 
-client.login(process.env.DISCORDJS_BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
